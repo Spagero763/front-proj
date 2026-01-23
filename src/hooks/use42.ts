cbc00a15f3a44f1fd6c86bc9f6a62c7dcd338945
Developer <dev@project.local>
2026-01-23 02:54:09 +0100

    feat: add use42 custom hook

---
+++ src/hooks/use42.ts
@@ -0,0 +1,8 @@
+﻿// use42 hook
+import { useState, useCallback } from 'react';
+
+export const use42 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
