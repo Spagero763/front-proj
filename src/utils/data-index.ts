897f9cb07846a92e0a47011a64bb52759a387483
Developer <dev@project.local>
2026-01-23 02:53:48 +0100

    feat: add data index utilities

---
+++ src/utils/data-index.ts
@@ -0,0 +1,4 @@
+﻿// index utilities
+export const isindex = (val: any): boolean => typeof val === 'object';
+export const getindex = (): any => ({});
+export const setindex = (val: any): void => {};
