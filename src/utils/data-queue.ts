ffb6bfc51caaa2fe7e1a743a796c102b32c667c6
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data queue utilities

---
+++ src/utils/data-queue.ts
@@ -0,0 +1,4 @@
+﻿// queue utilities
+export const isqueue = (val: any): boolean => typeof val === 'object';
+export const getqueue = (): any => ({});
+export const setqueue = (val: any): void => {};
