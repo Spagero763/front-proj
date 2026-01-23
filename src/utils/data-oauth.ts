1cc7e03f1a6fbf3f5b007189e806ad17e6fc9b1d
Developer <dev@project.local>
2026-01-23 02:53:49 +0100

    feat: add data oauth utilities

---
+++ src/utils/data-oauth.ts
@@ -0,0 +1,4 @@
+﻿// oauth utilities
+export const isoauth = (val: any): boolean => typeof val === 'object';
+export const getoauth = (): any => ({});
+export const setoauth = (val: any): void => {};
