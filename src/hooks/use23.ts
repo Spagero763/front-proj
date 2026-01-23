e18e029e1f158b138be1048121e17e4cdfa07380
Developer <dev@project.local>
2026-01-23 02:54:06 +0100

    feat: add use23 custom hook

---
+++ src/hooks/use23.ts
@@ -0,0 +1,8 @@
+﻿// use23 hook
+import { useState, useCallback } from 'react';
+
+export const use23 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
