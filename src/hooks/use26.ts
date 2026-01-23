66039c1e717472ccd489b90412b6abe86dcda3fb
Developer <dev@project.local>
2026-01-23 02:54:07 +0100

    feat: add use26 custom hook

---
+++ src/hooks/use26.ts
@@ -0,0 +1,8 @@
+﻿// use26 hook
+import { useState, useCallback } from 'react';
+
+export const use26 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
