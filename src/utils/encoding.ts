a09ccd5a10364afb6880ad34b8cbe59e5ce9be22
Developer <dev@project.local>
2026-01-23 02:52:41 +0100

    feat: add encoding utility

---
+++ src/utils/encoding.ts
@@ -0,0 +1,7 @@
+﻿// Encoding and decoding utilities
+export const btoa = (str: string): string => Buffer.from(str, 'utf8').toString('base64');
+export const atob = (str: string): string => Buffer.from(str, 'base64').toString('utf8');
+export const encodeURI = (str: string): string => globalThis.encodeURI(str);
+export const decodeURI = (str: string): string => globalThis.decodeURI(str);
+export const encodeURIComponent = (str: string): string => globalThis.encodeURIComponent(str);
+export const decodeURIComponent = (str: string): string => globalThis.decodeURIComponent(str);
