3202c0b50a71aaa7f5350883a1f4fdfed0e82913
Developer <dev@project.local>
2026-01-23 02:54:10 +0100

    feat: add use45 custom hook

---
+++ src/hooks/use45.ts
@@ -0,0 +1,8 @@
+﻿// use45 hook
+import { useState, useCallback } from 'react';
+
+export const use45 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
