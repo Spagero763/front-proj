c36378d86ee54e2373fcb6cf08390f9535e7a291
Developer <dev@project.local>
2026-01-23 02:54:07 +0100

    feat: add use28 custom hook

---
+++ src/hooks/use28.ts
@@ -0,0 +1,8 @@
+﻿// use28 hook
+import { useState, useCallback } from 'react';
+
+export const use28 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
