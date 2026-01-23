29fb3349f8631f16e9eaca5602209dafe6652dfe
Developer <dev@project.local>
2026-01-23 02:54:08 +0100

    feat: add use33 custom hook

---
+++ src/hooks/use33.ts
@@ -0,0 +1,8 @@
+﻿// use33 hook
+import { useState, useCallback } from 'react';
+
+export const use33 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
