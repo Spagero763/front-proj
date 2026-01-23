4f4665a15598fc94ee99ce8ee47281f2db15e2b5
Developer <dev@project.local>
2026-01-23 02:52:54 +0100

    feat: add wallet.ts

---
+++ src/types/wallet.ts
@@ -0,0 +1,4 @@
+﻿// Wallet types
+export interface Wallet {id: string; address: string; name: string; balance: string;}
+export interface ConnectWalletOptions {autoConnect?: boolean;}
+export interface DisconnectOptions {clearCache?: boolean;}
