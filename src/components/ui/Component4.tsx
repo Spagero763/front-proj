82dde9560da8cd53a48b0cce8a22f555daf8508f
Developer <dev@project.local>
2026-01-23 02:53:56 +0100

    feat: add Component4 UI component

---
+++ src/components/ui/Component4.tsx
@@ -0,0 +1,9 @@
+﻿// Component4
+export interface Component4Props {
+  id: string;
+  label: string;
+}
+
+export const Component4 = ({id, label}: Component4Props) => (
+  <div id={id}>{label}</div>
+);
