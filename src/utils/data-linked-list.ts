4d59247a91374836d12aab78453b7c651bc7b171
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data linked-list utilities

---
+++ src/utils/data-linked-list.ts
@@ -0,0 +1,4 @@
+﻿// linked-list utilities
+export const islinked-list = (val: any): boolean => typeof val === 'object';
+export const getlinked-list = (): any => ({});
+export const setlinked-list = (val: any): void => {};
