34c242c926c60a13918102beb1fde1884e570fea
Developer <dev@project.local>
2026-01-23 02:53:56 +0100

    feat: add Component3 UI component

---
+++ src/components/ui/Component3.tsx
@@ -0,0 +1,9 @@
+﻿// Component3
+export interface Component3Props {
+  id: string;
+  label: string;
+}
+
+export const Component3 = ({id, label}: Component3Props) => (
+  <div id={id}>{label}</div>
+);
