a76ba7e0f0aed5c3939f419123be6293eb4fcc5a
Developer <dev@project.local>
2026-01-23 02:53:48 +0100

    feat: add data pagination utilities

---
+++ src/utils/data-pagination.ts
@@ -0,0 +1,4 @@
+﻿// pagination utilities
+export const ispagination = (val: any): boolean => typeof val === 'object';
+export const getpagination = (): any => ({});
+export const setpagination = (val: any): void => {};
