8d99efc33c8b53084e24bd0a9128d8d27c22989b
Developer <dev@project.local>
2026-01-23 02:53:12 +0100

    feat: add token.ts

---
+++ src/services/token.ts
@@ -0,0 +1,3 @@
+﻿// Token service
+export const getTokenInfo = async (address: string) => ({address, symbol: "TOKEN", decimals: 18});
+export const transferToken = async (to: string, amount: string) => ({success: true});
