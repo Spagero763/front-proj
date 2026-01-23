20cc9e4e7b27edf523343129fcd84d3234d3c63f
Developer <dev@project.local>
2026-01-23 02:53:59 +0100

    feat: add Component24 UI component

---
+++ src/components/ui/Component24.tsx
@@ -0,0 +1,9 @@
+﻿// Component24
+export interface Component24Props {
+  id: string;
+  label: string;
+}
+
+export const Component24 = ({id, label}: Component24Props) => (
+  <div id={id}>{label}</div>
+);
