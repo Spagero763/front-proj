302268f8f029ccefffb0c1f66bfb8d24cbb6e5f1
Developer <dev@project.local>
2026-01-23 02:53:57 +0100

    feat: add Component8 UI component

---
+++ src/components/ui/Component8.tsx
@@ -0,0 +1,9 @@
+﻿// Component8
+export interface Component8Props {
+  id: string;
+  label: string;
+}
+
+export const Component8 = ({id, label}: Component8Props) => (
+  <div id={id}>{label}</div>
+);
