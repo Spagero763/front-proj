9b88c9b88353a80fce132a6d77ee22dcaa8ecacd
Developer <dev@project.local>
2026-01-23 02:53:49 +0100

    feat: add data validation-advanced utilities

---
+++ src/utils/data-validation-advanced.ts
@@ -0,0 +1,4 @@
+﻿// validation-advanced utilities
+export const isvalidation-advanced = (val: any): boolean => typeof val === 'object';
+export const getvalidation-advanced = (): any => ({});
+export const setvalidation-advanced = (val: any): void => {};
