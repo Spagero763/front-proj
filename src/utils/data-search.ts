acbf7ff4ef9e1fdb17d07bda1458467127fad8f4
Developer <dev@project.local>
2026-01-23 02:53:48 +0100

    feat: add data search utilities

---
+++ src/utils/data-search.ts
@@ -0,0 +1,4 @@
+﻿// search utilities
+export const issearch = (val: any): boolean => typeof val === 'object';
+export const getsearch = (): any => ({});
+export const setsearch = (val: any): void => {};
