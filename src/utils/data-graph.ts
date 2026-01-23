0a7d890c78a337449f3a5b66cf8018333ae2dd54
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data graph utilities

---
+++ src/utils/data-graph.ts
@@ -0,0 +1,4 @@
+﻿// graph utilities
+export const isgraph = (val: any): boolean => typeof val === 'object';
+export const getgraph = (): any => ({});
+export const setgraph = (val: any): void => {};
