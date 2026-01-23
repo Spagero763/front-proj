0882300a223321a2fc6f07c024fbe5d6f296a740
Developer <dev@project.local>
2026-01-23 02:54:09 +0100

    feat: add use39 custom hook

---
+++ src/hooks/use39.ts
@@ -0,0 +1,8 @@
+﻿// use39 hook
+import { useState, useCallback } from 'react';
+
+export const use39 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
