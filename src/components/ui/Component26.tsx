41e2f85f2f2815c4be97a28c48e670b0a12aac85
Developer <dev@project.local>
2026-01-23 02:54:00 +0100

    feat: add Component26 UI component

---
+++ src/components/ui/Component26.tsx
@@ -0,0 +1,9 @@
+﻿// Component26
+export interface Component26Props {
+  id: string;
+  label: string;
+}
+
+export const Component26 = ({id, label}: Component26Props) => (
+  <div id={id}>{label}</div>
+);
