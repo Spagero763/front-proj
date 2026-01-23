76ea0faadd8e2d1d09c65857ae54d9494fc015e5
Developer <dev@project.local>
2026-01-23 02:54:06 +0100

    feat: add use22 custom hook

---
+++ src/hooks/use22.ts
@@ -0,0 +1,8 @@
+﻿// use22 hook
+import { useState, useCallback } from 'react';
+
+export const use22 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
