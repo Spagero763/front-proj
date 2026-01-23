1f897ea658143d778d1a6761e630cbf1c2a5166b
Developer <dev@project.local>
2026-01-23 02:52:33 +0100

    feat: add promise utility

---
+++ src/utils/promise.ts
@@ -0,0 +1,8 @@
+﻿// Promise utilities
+export const delay = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
+export const timeout = <T>(promise: Promise<T>, ms: number): Promise<T> => {
+  return Promise.race([promise, new Promise<T>((_, reject) => setTimeout(() => reject(new Error('Timeout')), ms))]);
+};
+export const all = Promise.all;
+export const race = Promise.race;
+export const allSettled = Promise.allSettled;
