f928664b983455c192256fbf8e4d97fc53542537
Developer <dev@project.local>
2026-01-23 02:54:09 +0100

    feat: add use40 custom hook

---
+++ src/hooks/use40.ts
@@ -0,0 +1,8 @@
+﻿// use40 hook
+import { useState, useCallback } from 'react';
+
+export const use40 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
