6bcd0c98a89c90876a9822508038f2220798cb27
Developer <dev@project.local>
2026-01-23 02:54:08 +0100

    feat: add use34 custom hook

---
+++ src/hooks/use34.ts
@@ -0,0 +1,8 @@
+﻿// use34 hook
+import { useState, useCallback } from 'react';
+
+export const use34 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
