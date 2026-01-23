33622a7ec6def48edc24b809903f7b2772e7612d
Developer <dev@project.local>
2026-01-23 02:53:56 +0100

    feat: add Component2 UI component

---
+++ src/components/ui/Component2.tsx
@@ -0,0 +1,9 @@
+﻿// Component2
+export interface Component2Props {
+  id: string;
+  label: string;
+}
+
+export const Component2 = ({id, label}: Component2Props) => (
+  <div id={id}>{label}</div>
+);
