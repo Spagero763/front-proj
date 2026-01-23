1de6e0f75459f3d53ceeee600a91f0f4a44e304e
Developer <dev@project.local>
2026-01-23 02:53:59 +0100

    feat: add Component20 UI component

---
+++ src/components/ui/Component20.tsx
@@ -0,0 +1,9 @@
+﻿// Component20
+export interface Component20Props {
+  id: string;
+  label: string;
+}
+
+export const Component20 = ({id, label}: Component20Props) => (
+  <div id={id}>{label}</div>
+);
