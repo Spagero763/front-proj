f308da25a9f6e5429d7ea851f65119eff0eb28b1
Developer <dev@project.local>
2026-01-23 02:53:57 +0100

    feat: add Component12 UI component

---
+++ src/components/ui/Component12.tsx
@@ -0,0 +1,9 @@
+﻿// Component12
+export interface Component12Props {
+  id: string;
+  label: string;
+}
+
+export const Component12 = ({id, label}: Component12Props) => (
+  <div id={id}>{label}</div>
+);
