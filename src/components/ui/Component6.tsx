aa195753999d522d50c07cab3806294a1d59c43c
Developer <dev@project.local>
2026-01-23 02:53:57 +0100

    feat: add Component6 UI component

---
+++ src/components/ui/Component6.tsx
@@ -0,0 +1,9 @@
+﻿// Component6
+export interface Component6Props {
+  id: string;
+  label: string;
+}
+
+export const Component6 = ({id, label}: Component6Props) => (
+  <div id={id}>{label}</div>
+);
