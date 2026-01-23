84d6614452eea343885fe64f05a0c9a6fcb893ea
Developer <dev@project.local>
2026-01-23 02:54:06 +0100

    feat: add use24 custom hook

---
+++ src/hooks/use24.ts
@@ -0,0 +1,8 @@
+﻿// use24 hook
+import { useState, useCallback } from 'react';
+
+export const use24 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
