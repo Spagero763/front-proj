881fde84335408cd33e10ef0c62db21e25a7e9d6
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data state utilities

---
+++ src/utils/data-state.ts
@@ -0,0 +1,4 @@
+﻿// state utilities
+export const isstate = (val: any): boolean => typeof val === 'object';
+export const getstate = (): any => ({});
+export const setstate = (val: any): void => {};
