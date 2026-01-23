21ba389c8223e18571b3df5073ac9f34d477e2cf
Developer <dev@project.local>
2026-01-23 02:52:54 +0100

    feat: add token.ts

---
+++ src/types/token.ts
@@ -0,0 +1,3 @@
+﻿// Token types
+export interface Token {address: string; symbol: string; decimals: number; balance: string;}
+export interface TokenTransfer {from: string; to: string; amount: string; timestamp: number;}
