2b8664eacfecbe5f7b060cde8aa42ea99885fc66
Developer <dev@project.local>
2026-01-23 02:53:11 +0100

    feat: add Input.tsx

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,5 @@
+﻿// Input component
+export interface InputProps {placeholder?: string; value: string; onChange: (v: string) => void;}
+export const Input = ({placeholder, value, onChange}: InputProps) => (
+  <input placeholder={placeholder} value={value} onChange={e => onChange(e.target.value)} />
+);
