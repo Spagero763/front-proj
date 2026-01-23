ad33f17fb3954ca2d3e019e6d3f903a0a1ffa948
Developer <dev@project.local>
2026-01-23 02:52:54 +0100

    feat: add transaction.ts

---
+++ src/types/transaction.ts
@@ -0,0 +1,3 @@
+﻿// Transaction types
+export interface Transaction {hash: string; status: "pending"|"success"|"failed"; from: string; to: string;}
+export interface TransactionReceipt {transactionHash: string; status: number; blockNumber: number;}
