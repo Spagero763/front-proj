e2997eac2063c6dbc0eadaccd145aeabfb53f77d
Developer <dev@project.local>
2026-01-23 02:54:08 +0100

    feat: add use36 custom hook

---
+++ src/hooks/use36.ts
@@ -0,0 +1,8 @@
+﻿// use36 hook
+import { useState, useCallback } from 'react';
+
+export const use36 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
