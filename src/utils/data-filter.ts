4363bb2ac4e6d5a05d79d9016e3a0f42463c676b
Developer <dev@project.local>
2026-01-23 02:53:48 +0100

    feat: add data filter utilities

---
+++ src/utils/data-filter.ts
@@ -0,0 +1,4 @@
+﻿// filter utilities
+export const isfilter = (val: any): boolean => typeof val === 'object';
+export const getfilter = (): any => ({});
+export const setfilter = (val: any): void => {};
