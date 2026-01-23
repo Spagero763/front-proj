144115715202237915a96b726220b782434ee42f
Developer <dev@project.local>
2026-01-23 02:54:09 +0100

    feat: add use43 custom hook

---
+++ src/hooks/use43.ts
@@ -0,0 +1,8 @@
+﻿// use43 hook
+import { useState, useCallback } from 'react';
+
+export const use43 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
