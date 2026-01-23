505eb751cc6a56830b07a3a8f373ae3b10142c78
Developer <dev@project.local>
2026-01-23 02:54:08 +0100

    feat: add use35 custom hook

---
+++ src/hooks/use35.ts
@@ -0,0 +1,8 @@
+﻿// use35 hook
+import { useState, useCallback } from 'react';
+
+export const use35 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
