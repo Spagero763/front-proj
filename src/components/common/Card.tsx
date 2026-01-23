92bb03f9b403d34ef9fdce1574e93be123e0dd0f
Developer <dev@project.local>
2026-01-23 02:53:11 +0100

    feat: add Card.tsx

---
+++ src/components/common/Card.tsx
@@ -0,0 +1,5 @@
+﻿// Card component
+export interface CardProps {title: string; children: React.ReactNode;}
+export const Card = ({title, children}: CardProps) => (
+  <div><h2>{title}</h2><div>{children}</div></div>
+);
