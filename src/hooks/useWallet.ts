b5b1f53eb3c1d6610f75d314da70f03a733a50a1
Developer <dev@project.local>
2026-01-23 02:52:55 +0100

    feat: add useWallet.ts

---
+++ src/hooks/useWallet.ts
@@ -0,0 +1,7 @@
+﻿// Wallet hook
+import {useCallback, useState} from "react";
+export const useWallet = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const connect = useCallback(async () => {setIsConnected(true)}, []);
+  return {isConnected, connect};
+};
