a695f423f9cb18f6208723b6f4771170397da4a4
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data session utilities

---
+++ src/utils/data-session.ts
@@ -0,0 +1,4 @@
+﻿// session utilities
+export const issession = (val: any): boolean => typeof val === 'object';
+export const getsession = (): any => ({});
+export const setsession = (val: any): void => {};
