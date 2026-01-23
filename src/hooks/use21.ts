22a9669af573d5cd395ec4c3899aa02df7f98711
Developer <dev@project.local>
2026-01-23 02:54:06 +0100

    feat: add use21 custom hook

---
+++ src/hooks/use21.ts
@@ -0,0 +1,8 @@
+﻿// use21 hook
+import { useState, useCallback } from 'react';
+
+export const use21 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
