cb48a1eb71217e1d51e4538e5e6577d3e62251e6
Developer <dev@project.local>
2026-01-23 02:53:50 +0100

    feat: add data stack utilities

---
+++ src/utils/data-stack.ts
@@ -0,0 +1,4 @@
+﻿// stack utilities
+export const isstack = (val: any): boolean => typeof val === 'object';
+export const getstack = (): any => ({});
+export const setstack = (val: any): void => {};
