714723d58881e8542ce8d58880aaa67746f36b68
Developer <dev@project.local>
2026-01-23 02:54:09 +0100

    feat: add use41 custom hook

---
+++ src/hooks/use41.ts
@@ -0,0 +1,8 @@
+﻿// use41 hook
+import { useState, useCallback } from 'react';
+
+export const use41 = (initialValue: any = null) => {
+  const [state, setState] = useState(initialValue);
+  const update = useCallback((newValue: any) => setState(newValue), []);
+  return { state, update, setState };
+};
