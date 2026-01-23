ec9c1b5326a42b4e430c149a5555a457e5e41262
Developer <dev@project.local>
2026-01-23 02:52:55 +0100

    feat: add useBalance.ts

---
+++ src/hooks/useBalance.ts
@@ -0,0 +1,7 @@
+﻿// Balance hook
+import {useState, useEffect} from "react";
+export const useBalance = () => {
+  const [balance, setBalance] = useState("0");
+  useEffect(() => {setBalance("0")}, []);
+  return balance;
+};
