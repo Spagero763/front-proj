f2e23c67a68b0da113ea3f0f01bdcd478c36e3dc
Developer <dev@project.local>
2026-01-23 02:53:49 +0100

    feat: add data compression utilities

---
+++ src/utils/data-compression.ts
@@ -0,0 +1,4 @@
+﻿// compression utilities
+export const iscompression = (val: any): boolean => typeof val === 'object';
+export const getcompression = (): any => ({});
+export const setcompression = (val: any): void => {};
