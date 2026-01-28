b03341bd8d1146cc56b8c5e14c68c645f8436e28
Spagero 763 <afolabiayomide870@gmail.com>
2026-01-28 17:48:22 +0100

    fix(urls): update explorer to basescan and add helper functions

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,6 +1,15 @@
 ﻿// URL configuration
 export const urls = {
-  explorer: "https://etherscan.io",
-  docs: "https://docs.own.protocol",
-  github: "https://github.com/ownprotocol",
+  explorer: "https://sepolia.basescan.org",
+  docs: "https://own-protocol.gitbook.io/docs",
+  github: "https://github.com/own-protocol",
+  twitter: "https://x.com/iownco",
+  telegram: "https://t.me/+EX6VZh6rrPc5YmI9",
+};
+
+// Helper to generate block explorer URLs
+export const getExplorerUrl = {
+  tx: (hash: string) => `${urls.explorer}/tx/${hash}`,
+  address: (addr: string) => `${urls.explorer}/address/${addr}`,
+  token: (addr: string) => `${urls.explorer}/token/${addr}`,
 };
